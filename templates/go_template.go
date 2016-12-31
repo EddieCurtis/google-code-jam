5a1bc436455cce22b389554421db3ce8bd2235f4
EddieCurtis <e.j.curtis1@gmail.com>
2016-12-30 21:27:56 -0500

    Updated Go template

--- templates/go_template.go
+++ templates/go_template.go
@@ -15,14 +15,16 @@ func main() {
 	} else {
 		lines := strings.Split(string(data), "\n")
 		// Remove first value as that just defines number of input lines
-		for _, next := range lines[1:] {
+		for i, next := range lines[1:] {
 			if next != "" {
-				processLine(next)
+				result := processLine(next)
+				fmt.Printf("Case #%d: %s\n", i+1, result)
 			}
 		}
 	}
 }
 
 func processLine(line string) {
-	fmt.Println(line)
+	// Implement solution here
+	return ""
 }
